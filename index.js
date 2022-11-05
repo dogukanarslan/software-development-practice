6609a78382af4403ca343b08002bdfd555a8ef39
Dogukan Arslan <dogukan2arslan@hotmail.com>
2022-11-05 20:42:43 +0300

    Connect to database

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.listen(3000)
-
+// View engine
 app.set('view engine', 'ejs');
 
-app.use(express.json())
+// Database connection
+dbURI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.b9onbdw.mongodb.net/${process.env.DB}`;
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
 
 app.use(authRoutes);
