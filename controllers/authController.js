eaa30688350b735bf1bc7fb9cc13c67b2bbafdc8
Dogukan Arslan <dogukan2arslan@hotmail.com>
2022-11-06 10:36:19 +0300

    Add login and signup views

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,10 @@ const handleErrors = (err) => {
   return errors;
 };
 
+module.exports.signup_get = (req, res) => {
+  res.render('signup');
+};
+
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
@@ -30,6 +34,10 @@ module.exports.signup_post = async (req, res) => {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
+};
+
+module.exports.login_get = (req, res) => {
+  res.render('login');
 };
 
 module.exports.login_post = (req, res) => {
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(express.static('public'));
+
 // View engine
 app.set('view engine', 'ejs');
 
