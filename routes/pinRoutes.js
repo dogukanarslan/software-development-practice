6c4f70a9b782ac7321918e86c300e1a66a4e8e0a
Dogukan Arslan <dogukan2arslan@hotmail.com>
2022-11-22 21:54:09 +0300

    Rearrange route order

--- routes/pinRoutes.js
+++ routes/pinRoutes.js
@@ -4,10 +4,11 @@ const requireAuth = require('../middlewares/authMiddleware');
 
 const router = Router();
 
-router.get('/pins', pinController.list_get);
-router.get('/pins/:pinId', pinController.show_get);
-
 router.get('/pins/create', pinController.create_get);
 router.post('/pins/create', pinController.create_post);
+
+router.get('/pins', pinController.list_get);
+
+router.get('/pins/:pinId', pinController.show_get);
 
 module.exports = router;
